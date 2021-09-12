9c3ee00a18f2e4c4fa15b9bc557dfcedf369b348
OsaPL <osa12pl@gmail.com>
2021-09-12 10:14:23 +0200

    First test

--- src/Plugin/src/Hooks.cs
+++ src/Plugin/src/Hooks.cs
@@ -1,15 +1,19 @@
+using System;
+using On.FistVR;
+
 namespace H3VRMod
 {
 	public class Hooks
 	{
+		private readonly Sosig.hook_CanCurrentlyBeHeld _override =(orig, self) => true;
 		public void Hook()
 		{
-
+			On.FistVR.Sosig.CanCurrentlyBeHeld += _override;
 		}
 
 		public void Unhook()
 		{
-
+			On.FistVR.Sosig.CanCurrentlyBeHeld -= _override;
 		}
 	}
 }
